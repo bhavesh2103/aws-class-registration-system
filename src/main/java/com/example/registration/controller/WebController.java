0207acdb14cb476770c19f9ed675be798be95b84
bhavesh <bhavesh.khubnani@gmail.com>
2023-11-04 14:23:06 -0700

    working

--- src/main/java/com/example/registration/controller/WebController.java
+++ src/main/java/com/example/registration/controller/WebController.java
@@ -2,13 +2,16 @@ package com.example.registration.controller;
 
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WebController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Greetings from Spring Boot!";
+    public String hello(@RequestParam(name="name", required=false, defaultValue="User") String name, Model model) {
+        model.addAttribute("name", name);
+        return "Greetings "+name+" from Spring Boot!";
     }
 }
